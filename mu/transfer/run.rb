0e2bfc2dc0cbe59db93a6919a405571b37bae3fa
hang <hang22c@gmail.com>
2017-06-08 01:57:04 +0800

    CHG update transfer

--- mu/transfer/run.rb
+++ mu/transfer/run.rb
@@ -33,7 +33,7 @@ def get_transfer_tweets(client)
     posts.each do |post|
       body = post.css('blockquote')
       link = URL_BASE + post.css('.hashPermalink').attribute('href').text
-    #   next if seen_posts.include?(link)
+      next if seen_posts.include?(link)
       puts(link)
       main_text = body.css('.messageText').css(' > text()').text.strip.presence
       tweet_urls = post.children.css('.twitter-tweet').children.css('a').map { |t| t.attribute('href').text }
@@ -42,10 +42,9 @@ def get_transfer_tweets(client)
           puts(url)
           tweet = client.status(url)
           tweet_data = (data[tweet.id] || {}).merge(tweet.attrs)
-          p tweet_data['comments']
-          p '___'
-          tweet_data['comments'] = (tweet_data['comments'] || []).append(main_text).uniq.compact
-          p tweet_data['comments']
+          comments = (tweet_data['comments'] || [])
+          comments.append(main_text)
+          tweet_data['comments'] = comments.uniq.compact
           tweet_data['floor'] ||= post.css('.hashPermalink').text.gsub('#', '')
           tweet_data['link'] ||= link
           tweet_data['text'] = tweet.full_text
