4b3ec046959715481b64eff4a93843f31eaed410
hang <hang22c@gmail.com>
2017-07-06 02:23:24 +0800

    ADD more pass visualization

--- hupu/visualisation/team_passes.js
+++ hupu/visualisation/team_passes.js
@@ -1,16 +1,3 @@
-var teamsData, teamNames, selectTeam,
-	TABLE_HEADERS = ['Name'];
-d3.json("passes.json", function(data) {
-	teamsData = data;
-	teamNames = [];
-	Object.keys(teamsData).forEach(function(key, index) {
-		teamNames.push({id: key, nameUniq: key,  name: key })
-	});
-	selectTeam = 'GSW'
-	drawTable();
-	drawCombinedPasses(data[selectTeam].combined)
-});
-
 function searchBy(event) {
     if (event.keyCode == 13) {
         var teamName = document.getElementById("teamSearch").value.toLowerCase();
@@ -61,7 +48,7 @@ function drawTable() {
                 selectTeam = d.nameUniq;
 								console.log(d)
 								document.getElementsByClassName('teamPassesTitle')[0].innerHTML = 'Team Passes: ' + d.name;
-								drawCombinedPasses(teamsData[selectTeam].combined)
+								drawWithData(teamsData[selectTeam].combined)
             }
         });
     rows.selectAll('td')
@@ -297,4 +284,99 @@ function drawCombinedPasses(json) {
 			});
 	}//wrap
 
+}
+
+function drawForceDirect(data) {
+	// get the data
+	var nodes = data.nodes;
+	var links = data.links;
+
+
+	// Compute the distinct nodes from the links.
+	links.forEach(function(link) {
+	    link.source = nodes[link.source] ||
+	        (nodes[link.source] = {name: link.source});
+	    link.target = nodes[link.target] ||
+	        (nodes[link.target] = {name: link.target});
+	    link.value = +link.value;
+	});
+
+	var width = 800,
+	    height = 800;
+
+	var force = d3.layout.force()
+	    .nodes(nodes)
+	    .links(links)
+	    .size([width, height])
+	    .linkDistance(400)
+	    .charge(-500)
+	    .on("tick", tick)
+	    .start();
+	d3.select('.passLinks').remove()
+	var svg = d3.select(".linksWrapper")
+			.append("svg")
+			.attr('class', 'passLinks')
+	    .attr("width", width)
+	    .attr("height", height);
+
+	// build the arrow.
+	svg.append("svg:defs").selectAll("marker")
+	    .data(["end"])      // Different link/path types can be defined here
+	  .enter().append("svg:marker")    // This section adds in the arrows
+	    .attr("id", String)
+	    .attr("viewBox", "0 -5 10 10")
+	    .attr("refX", 12)
+	    .attr("refY", -0.5)
+	    .attr("markerWidth", 6)
+	    .attr("markerHeight", 6)
+	    .attr("orient", "auto")
+	  .append("svg:path")
+	    .attr("d", "M0,-5L10,0L0,5")
+	;
+
+	// add the links and the arrows
+	var path = svg.append("svg:g").selectAll("path")
+	    .data(force.links())
+	  .enter().append("svg:path")
+	  //  .attr("class", function(d) { return "link " + d.type; })
+	    .attr("class", "link")
+			.style('stroke-width', function(d){ return d.time/250} )
+			.style('stroke-color', 'blue')
+	    .attr("marker-end", "url(#end)");
+
+	// define the nodes
+	var node = svg.selectAll(".node")
+	    .data(force.nodes())
+	  .enter().append("g")
+	    .attr("class", "node")
+	    .call(force.drag);
+
+	// add the nodes
+	node.append("circle")
+	    .attr("r", 5);
+
+	// add the text
+	node.append("text")
+	    .attr("x", 12)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.name; });
+
+	// add the curvy lines
+	function tick() {
+	    path.attr("d", function(d) {
+	        var dx = d.target.x - d.source.x,
+	            dy = d.target.y - d.source.y,
+	            dr = Math.sqrt(dx * dx + dy * dy);
+	        return "M" +
+	            d.source.x + "," +
+	            d.source.y + "A" +
+	            dr + "," + dr + " 0 0,1 " +
+	            d.target.x + "," +
+	            d.target.y;
+	    });
+
+	    node
+	        .attr("transform", function(d) {
+	  	    return "translate(" + d.x + "," + d.y + ")"; });
+	}
 }
