168bbe60f784871fec0521f95a0393373254fea1
hang <hang22c@gmail.com>
2017-07-21 18:29:06 +0800

    ADD compare

--- hupu/sklearn_kmeans.py
+++ hupu/sklearn_kmeans.py
@@ -28,7 +28,7 @@ HEADERS = [
 ]
 
 # DATA_PATH = 'play_types.data'
-DATA_PATH = '../csv/league_data.csv'
+DATA_PATH = '../csv/defensive_league_data.csv'
 PATH = 'cluster_img'
 
 
@@ -139,8 +139,8 @@ def show_result_sc(data):
 
 
 # show_result(data)
-# show_result_elbow(data)
+show_result_elbow(data)
 # ap(data)
-cengci(data)
+# cengci(data)
 # get_images(16, data)
 # team_cluster()
