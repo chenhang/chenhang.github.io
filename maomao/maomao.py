da36b6c6beab51b664b8004f9531c24aa6e1a415
Hang CHEN <hang22c@gmail.com>
2018-02-27 17:04:49 -0500

    ADD new maomao website

--- maomao/maomao.py
+++ maomao/maomao.py
@@ -26,11 +26,11 @@ HEADERS = {
 }
 
 BASE_URL = "http://www.hoobly.com"
-OODLE_BASE_URL = ""
+OODLE_BASE_URL = "https://cats.oodle.com"
 
 
 def api(start=0):
-    return "http://www.hoobly.com/12030/1940/" + str(start)+"/"
+    return "http://www.hoobly.com/12030/1940/" + str(start) + "/"
 
 
 def oodle_api(start=0):
@@ -85,37 +85,51 @@ def get_oodle_maomaos():
         print(start)
         if start > 100:
             break
-        res = get(api(start=start), headers=HEADERS, timeout=50)
+        res = get(oodle_api(start=start), headers=HEADERS, timeout=50)
         start += 15
-        table = BeautifulSoup(res.content, "html.parser").find('table')
-        trs = table.findAll('tr')
+        table = BeautifulSoup(res.content, "html.parser").find('ol')
+        trs = table.findAll('li', {'class': 'has-thumbnail'})
         for tr in trs:
-            img_url = to_img_full_url(tr.find('img').attrs['src'])
-            detail_url = BASE_URL + tr.find('a').attrs['href']
-            desc, _, location, _, breed, _ = tr.find(
-                'div').text.strip().split('\n\t\t\t\t\t\t\t')
+            detail_url = OODLE_BASE_URL + \
+                tr.find('a', {'class': 'title-link'}).attrs['href']
             detail_res = get(detail_url, headers=HEADERS, timeout=50)
+            detail_url = detail_res.url
             soup = BeautifulSoup(detail_res.content, "html.parser")
-            detail_table, info_table, _ = soup.findAll('table')
-            img_tr, content_tr = detail_table.findAll("tr") if len(
-                detail_table.findAll("tr")) == 2 else [[]] + detail_table.findAll("tr")
-            info = [td.text.strip() for td in info_table.findAll("td")]
-            info_dict = dict(zip(info[0::2], info[1::2]))
-            posted = to_str_date(info_dict['Posted'])
-            print(desc)
-            img_urls = [to_img_full_url(img.attrs['src'])
-                        for img in img_tr.findAll('img')]
-            id = detail_url.split('/')[3]
+            content_div = soup.find('div', {'id': 'listing-detail-container'})
+            keys = [div.text.strip().replace(':', '').lower()
+                    for div in content_div.findAll('div', {'class': 'listing-attribute'})]
+            values = [div.text.strip() for div in content_div.findAll(
+                'div', {'class': 'listing-attribute-value'})]
+            info_dict = dict(zip(keys, values))
+
+            if content_div.find('div', {'id': 'photo-view'}):
+                img_urls = [img.attrs['src'] for img in content_div.find(
+                    'div', {'id': 'photo-view'}).findAll('img')]
+            else:
+                img_urls = [
+                    'https://ws1.sinaimg.cn/large/006tNc79gy1fovo1rjkghj305o05p40b.jpg']
+            print(info_dict['posted'])
+            date_list = info_dict['posted'].split()[:3] if '20' in info_dict['posted'].split()[
+                2] else info_dict['posted'].split()[:2] + ['2018']
+            if len(date_list) < 3:
+                date_list.append('2018')
+            info_dict['posted'] = datetime.datetime.strptime(
+                ' '.join(date_list), "%B %d %Y").date().strftime('%Y-%m-%d')
+
+            print(info_dict['description'])
+            id = '_'.join(['oodle', tr.attrs['id'].split('-')[-1]])
             if id in ids:
                 break
             ids.append(id)
-            maomaos.append({'id': detail_url.split('/')[3], 'title': tr.find('img').attrs['alt'],
-                            "desc": desc, 'location': location,
-                            'posted': to_str_date(info_dict['Posted']), 'updated': to_str_date(info_dict['Updated']),
-                            'breed': breed, 'price': tr.find('span').text, 'content': content_tr.text.strip(),
-                            'main_img_url': img_url, 'detail_url': detail_url, 'img_urls': img_urls})
+            maomao = {'id': id, 'title': tr.findAll('a')[1].text,
+                      'updated': info_dict['posted'], 'content': info_dict['description'],
+                      'main_img_url': img_urls[0], 'detail_url': detail_url, 'img_urls': img_urls}
+            for key, value in info_dict.items():
+                new_key = 'desc' if key == 'description' else key
+                maomao[new_key] = value
+            maomaos.append(maomao)
             time.sleep(2)
-        write_json("data/maomaos.json", maomaos)
+        write_json("data/oodle_maomaos.json", maomaos)
         leancloud_objects = [leancloud_object(
             "Maomao", maomao, id_key="id") for maomao in maomaos]
         leancloud.Object.save_all([leancloud_object(
@@ -174,3 +188,4 @@ def get_maomaos():
 
 if __name__ == '__main__':
     get_maomaos()
+    get_oodle_maomaos()
